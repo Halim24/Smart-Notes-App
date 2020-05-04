8c6f1fa3a34cff1eae07b703edbced2875ba6df3
Abdulhalim Karakelle <karakellehalim@gmail.com>
2020-05-04 11:36:02 +0300

    Add note icone
    Not eklemek için sol alta artı butonu eklendi

--- Akıllı Notlar/ContentView.swift
+++ Akıllı Notlar/ContentView.swift
@@ -11,9 +11,18 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
     ZStack {
-        Color(red: 254 / 255, green: 254 / 255, blue: 211 / 255).edgesIgnoringSafeArea(.all)
+        Color(red: 254 / 255, green: 253 / 255, blue: 210 / 255)
+        .edgesIgnoringSafeArea(.all)
             VStack {
-                Text("Hello, World!")
+            
+            
+                Image(systemName: "plus.circle.fill")
+                .resizable()
+                .frame(width: 90.0, height: 90.0)
+                .padding(.leading,290)
+                .padding(.top,750)
+                .foregroundColor(Color.green)
+                
             }
     }
     }
